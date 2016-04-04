c7c5f72838a30e466efe49050faacc941e510be1
Florianfohrer <florianfohrer@hotmail.com>
2016-04-04 21:36:36 +0200

    First commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Adalys</title>
+<title>Adalys hhhh</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Baxster Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
