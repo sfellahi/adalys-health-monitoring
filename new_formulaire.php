01f9f192316c946ec2a4a5e5ea07c61414af39db
FlavienL <flavien.levacher@gmail.com>
2016-06-07 14:35:15 +0200

    misse en forme + loader
    new formulaire et new onglet

--- new_formulaire.php
+++ new_formulaire.php
@@ -3,49 +3,67 @@
 include('html/mainheader.php');
 if(isset($_GET['parent']))
 {
-$id = intval($_GET['parent']);
-if(isset($_POST['formulaire']))
-{
-	$nom = $_POST['formulaire'];
-	if(mysqli_query($link2,'insert into formulaire (nom_formulaire) VALUES ("'.$nom.'")'))
+	$id = intval($_GET['parent']);
+	if(isset($_POST['formulaire']))
 	{
-		$dn2 = mysqli_query($link,'select id_formulaire from formulaire where nom_formulaire = "'.$nom.'"');
-		$dnn2 = mysqli_fetch_array($dn2);
-		if(mysqli_query($link,'insert into project_formulaire (id_project, id_formulaire) VALUES ("'.$id.'", "'.$dnn2['id_formulaire'].'")'))
-              
+		$nom = $_POST['formulaire'];
+		if(mysqli_query($link2,'insert into formulaire (nom_formulaire) VALUES ("'.$nom.'")'))
 		{
-                   echo $sql_create_table_formulaire="CREATE TABLE donneeprojet".$id."formulaire".$dnn2['id_formulaire']." (id INT PRIMARY KEY NOT NULL)";
-                   mysqli_query($link,$sql_create_table_formulaire);
-			?>
-			<div id="page-wrapper">
-			<div class="main-page">
-				<div class="message">Le formulaire a ete cree. <br />
-				</div></div></div>
-     <?php include("html/mainfooter.html");?>
-				<meta http-equiv="refresh" content="2; URL=list_project.php">
-	<?php
+			$dn2 = mysqli_query($link,'select id_formulaire from formulaire where nom_formulaire = "'.$nom.'"');
+			$dnn2 = mysqli_fetch_array($dn2);
+			if(mysqli_query($link,'insert into project_formulaire (id_project, id_formulaire) VALUES ("'.$id.'", "'.$dnn2['id_formulaire'].'")'))
+
+			{
+				echo $sql_create_table_formulaire="CREATE TABLE donneeprojet".$id."formulaire".$dnn2['id_formulaire']." (id INT PRIMARY KEY NOT NULL)";
+				mysqli_query($link,$sql_create_table_formulaire);
+				?>
+				<div id="page-wrapper">
+					<div class="main-page">
+						<div class="row">
+							<div class="flat-table" style="margin:0 auto;margin-top:15%;width:18%;height:170px">
+								<div class="message" style="text-align:center ; font-weight:600;font-size:16px">Le formulaire est en cours de création <br /><br />
+									<div class="showbox">
+										<div class="loader">
+											<svg class="circular" viewBox="25 25 50 50">
+												<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+											</svg>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<?php include("html/mainfooter.html");?>
+				<meta http-equiv="refresh" content="4; URL=list_project.php">
+				<?php
+			}
 		}
 	}
-}
 
-?>
-<div id="page-wrapper">
-			<div class="main-page">
-<form action="new_formulaire.php?parent=<?php echo $id; ?>" method="post">
-<input type="hidden" value="<?php echo $id; ?>" name="parent">
-	<div>
-        <label for="formulaire">Nom du formulaire:</label>
-        <input type="text" name="formulaire" />
-    </div>
+	?>
+	<div id="page-wrapper">
+		<div class="main-page">
+			<div class="row">
+				<div class="flat-table" style="margin:0 auto;margin-top:15%;width:15%;height:150px">
+					<form action="new_formulaire.php?parent=<?php echo $id; ?>" method="post">
+						<input type="hidden" value="<?php echo $id; ?>" name="parent">
+						<div style="text-align:center">
+							<label for="formulaire" style="font-weight:600;font-size:16px">Nom du formulaire:</label>
+							<br />
+							<input type="text" name="formulaire" />
+						</div>
 
-    
-    <div class="button">
-        <button type="submit">Créer le formulaire</button>
-    </div>
 
- </form>
- <?php
-}?>
-                        </div>
-</div>
-                            <?php include("html/mainfooter.html");?>
+						<div style="text-align:center">
+							<br />
+							<button class="button" type="submit">Créer le formulaire</button>
+						</div>
+
+					</form>
+<?php } ?>
+				</div>
+			</div>
+		</div>
+	</div>
+		<?php include("html/mainfooter.html");?>
--- new_onglet.php
+++ new_onglet.php
@@ -3,43 +3,66 @@
 include('html/mainheader.php');
 if(isset($_GET['parent']))
 {
-$id = intval($_GET['parent']);
-if(isset($_POST['onglet']))
-{
-	$onglet = $_POST['onglet'];
-	if(mysqli_query($link2,'insert into onglet (nom_onglet, id_formulaire) VALUES ("'.$onglet.'", "'.$id.'")'))
+	$id = intval($_GET['parent']);
+	if(isset($_POST['onglet']))
 	{
-		
+		$onglet = $_POST['onglet'];
+		if(mysqli_query($link2,'insert into onglet (nom_onglet, id_formulaire) VALUES ("'.$onglet.'", "'.$id.'")'))
+		{
+
 			?>
 			<div id="page-wrapper">
-			<div class="main-page">
-				<div class="message">L'onglet a été crée.<br />
-				</div></div></div>
-     <?php include("html/mainfooter.html");?>
-				<meta http-equiv="refresh" content="2; URL=list_onglet.php?parent=<?php echo $id;?>">
-	<?php
-		
+				<div class="main-page">
+					<div class="row">
+						<div class="flat-table" style="margin:0 auto;margin-top:15%;width:18%;height:170px">
+							<div class="message"style="text-align:center ; font-weight:600;font-size:16px">L'onglet est en cours de création.<br /><br />
+								<div class="showbox">
+									<div class="loader">
+										<svg class="circular" viewBox="25 25 50 50">
+											<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php include("html/mainfooter.html");?>
+			<meta http-equiv="refresh" content="4; URL=list_onglet.php?parent=<?php echo $id;?>">
+			<?php
+
+		}
 	}
-}
 
-?>
-<div id="page-wrapper">
-			<div class="main-page">
-<form action="new_onglet.php?parent=<?php echo $id; ?>" method="post">
-<input type="hidden" value="<?php echo $id; ?>" name="parent">
-	<div>
-        <label for="formulaire">Nom de l'onglet</label>
-        <input type="text" name="onglet" />
-    </div>
+	?>
+	<div id="page-wrapper">
+		<div class="main-page">
+			<div class="row">
+				<div class="flat-table" style="margin:0 auto;margin-top:15%;width:15%;height:150px">
 
-    
-    <div class="button">
-        <button type="submit">Créer l'onglet</button>
-    </div>
+					<form action="new_onglet.php?parent=<?php echo $id; ?>" method="post">
+						<input type="hidden" value="<?php echo $id; ?>" name="parent">
+					</br>
+					<div style="text-align:center">
+						<label for="formulaire" style="font-weight:600;font-size:16px" >Nom de l'onglet</label>
 
- </form>
- <?php
-}?>
-                        </div>
+						<input type="text" name="onglet" />
+
+					</div>
+				</br>
+
+				<div style="text-align:center">
+					<button class="button" type="submit">Créer l'onglet</button>
+				</div>
+
+			</form>
+		</div>
+		<?php
+	}
+	?>
+
 </div>
-                            <?php include("html/mainfooter.html");?>
+</div>
+</div>
+<?php include("html/mainfooter.html");?>
