78fe9b77c9696fbc2c623fd797b671a3ecd0b629
HentaiSensei <HentaiSensei@air-de-do>
2016-06-15 13:25:51 +0200

    Last

--- index.php
+++ index.php
@@ -3,6 +3,46 @@ include("html/mainheader.php");
 $sql_nombre_projet='SELECT id_project FROM projects';
         $result_recup_nb_projet = mysqli_query($link,$sql_nombre_projet);
          $nb_projet = mysqli_num_rows($result_recup_nb_projet);
+         
+         
+         $result = mysqli_query($link2,"SELECT MAX(id_formulaire) as maxi FROM formulaire");
+         $row = mysqli_fetch_array($result);
+         
+         $result2 = mysqli_query($link2,"SELECT MAX(id_project) as max FROM projects");
+         $row2 = mysqli_fetch_array($result2);
+          
+         $a = 0;
+         $b =0;
+         for($i=1;$i<= $row2['max'];$i++){
+         	for($y=1;$y<= $row['maxi'];$y++){
+         		$table = "donneeprojet".$i."formulaire".$y;
+         		$s = mysqli_query($link,"SHOW TABLES LIKE '".$table."'");
+         		if (mysqli_num_rows($s) == 1) {		
+         					$arry[$a] = $table;			
+         					$req= mysqli_query($link,"select nom,prenom from $table");				
+         					while($data = mysqli_fetch_array($req))
+         					{
+         						$nom_complet = $data['nom']."  ".$data['prenom'];				
+         						$ary[$b] = $nom_complet;
+         						$b++;
+         					
+         					}	
+         				}
+         			$a++;
+         			}
+         		}
+         
+         $result = array_unique($ary);
+         $c = count($result);
+         
+         $query = mysqli_query($link2,"SELECT count(*) as nb FROM users");
+         $row3 = mysqli_fetch_array($query);
+         $nb_users = $row3['nb'] -1 ;
+         
+         $query2 = mysqli_query($link2,"SELECT count(*) as nb_f FROM formulaire");
+         $row4 = mysqli_fetch_array($query2);
+         $nb_form = $row4['nb_f'];
+         
 ?>
 		<!-- //header-ends -->
 		<!-- main content start-->
@@ -33,8 +73,8 @@ $sql_nombre_projet='SELECT id_project FROM projects';
 								</div>
 							</div>
 							<div class="grid-right">
-								<h3>Patients <span>enregistrés</span></h3>
-								<p>452</p>
+								<h3>Formulaires<span>créés</span></h3>
+								<p><?php echo $nb_form; ?></p>
 							</div>
 							<div class="clearfix"> </div>
 						</div>
@@ -48,8 +88,8 @@ $sql_nombre_projet='SELECT id_project FROM projects';
 								</div>
 							</div>
 							<div class="grid-right">
-								<h3>Notre <span>status</span></h3>
-								<p>125</p>
+								<h3>Patients <span>enregistrés</span></h3>
+								<p><?php echo $c; ?></p>
 							</div>
 							<div class="clearfix"> </div>
 						</div>
@@ -62,8 +102,8 @@ $sql_nombre_projet='SELECT id_project FROM projects';
 								</div>
 							</div>
 							<div class="grid-right">
-								<h3>Membres <span>connectés</span></h3>
-								<p>6</p>
+								<h3>Membres <span>abonnées</span></h3>
+								<p><?php echo $nb_users; ?></p>
 							</div>
 							<div class="clearfix"> </div>
 						</div>
