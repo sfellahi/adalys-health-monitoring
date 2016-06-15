24a92a64199bf9639e0ee6c194b72938ff8ca902
HentaiSensei <HentaiSensei@air-de-do>
2016-06-15 12:54:37 +0200

    Last

--- recherche_projet.php
+++ recherche_projet.php
@@ -1,6 +1,9 @@
 <?php
 include("html/mainheader.php");
 $name = $_POST['nom'];
+$nom_c = explode("  ", $name);
+$nom = $nom_c[0];
+$prenom = $nom_c[1];
 
 $nb_proj = $_POST['projet'];
 
@@ -13,27 +16,25 @@ $row = mysqli_fetch_array($result);?>
 	$sql_formulaire = "SELECT project_formulaire.id_formulaire,project_formulaire.id_project, formulaire.nom_formulaire,etat_formulaire ";
 	$sql_formulaire .= "FROM project_formulaire LEFT JOIN formulaire ";
 	$sql_formulaire .= "ON project_formulaire.id_formulaire=formulaire.id_formulaire AND project_formulaire.id_project = ".$nb_proj."";
-	$a=0;
 	$requete = mysqli_query ( $link, $sql_formulaire );
 	while ( $row2 = mysqli_fetch_array ( $requete ) ) {
 		?><th><?php echo $row2['nom_formulaire'];?></th><?php		
-		$a++;
 	}
 	?>
-</tr><tr><td><?php echo $name;?></td>
+</tr><tr><td><?php echo $name;?></td><td></td>
 <?php
-$a=0;
 for($y=1;$y<= $row['maxi'];$y++){
 	$table = "donneeprojet".$nb_proj."formulaire".$y;
 	$s = mysqli_query($link,"SHOW TABLES LIKE '".$table."'");
 	if (mysqli_num_rows($s) == 1) {
-		$sql_formulaire = "SELECT formulaire.id_formulaire,formulaire.nom_formulaire,etat_formulaire ";
-		$sql_formulaire .= "FROM formulaire WHERE id_formulaire= ".$y."";
-		$requete = mysqli_query ( $link, $sql_formulaire );
-		while ( $row = mysqli_fetch_array ( $requete ) ) {
-				?><td><?php echo $row['nom_formulaire'];?></td></tr><?php
-			}
+		$query = mysqli_query($link,"SELECT id FROM $table WHERE prenom LIKE '%$prenom%' AND nom LIKE'%$nom%'");
+		$nb_resultats = mysqli_num_rows($query);
+		if($nb_resultats != 0)
+		{
+			?><td><i class="fa fa-check-square-o" aria-hidden="true"></i></td><?php
+		}
 	}
 }
 
 ?></table></div></div>
+<?php include("html/mainfooter.html");?>
